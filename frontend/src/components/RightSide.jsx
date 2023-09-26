8bdfff47867b423e23acd90d1a89a4d81a363489
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-09-26 16:55:06 +0200

    adjustment css

--- frontend/src/components/RightSide.jsx
+++ frontend/src/components/RightSide.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FaPhoneAlt, FaVideo, FaRocketchat} from 'react-icons/fa';
+import {AiOutlineMenuUnfold, AiOutlineMenuFold} from 'react-icons/ai';
 import FriendInfo from './FriendInfo';
 import Message from './Message';
 import MessageSend from './MessageSend';
@@ -19,72 +20,90 @@ const RightSide = props => {
   } = props;
 
   return (
-    <div className="col-9">
-      <div className="right-side">
-        <input type="checkbox" id="dot" />
-        <div className="row">
-          <div className="col-8">
-            <div className="message-send-show">
-              <div className="header">
-                <div className="image-name">
-                  <div className="image">
-                    <img src={`./image/${currentfriend.image}`} alt="" />
+    <div>
 
-                    {activeUser &&
-                      activeUser.length > 0 &&
-                      activeUser.some (u => u.userId === currentfriend._id)
-                      ? <div className="active-icon" />
-                      : ''}
+      <input type="checkbox" id="dot2" />
+      <div className="col-9">
+        <div className="right-side">
+          <input type="checkbox" id="dot" />
+          <div className="row">
+            <div className="col-8">
+              <div className="message-send-show">
+                <div className="header">
+                  <div className="image-name">
 
+                  <div className='icons'>
+                    <div className="icon">
+                      <label htmlFor="dot2"> <AiOutlineMenuUnfold /> </label>
+                    </div>
+                    </div>
+
+                    <div className="image">
+                      <img src={`./image/${currentfriend.image}`} alt="" />
+
+                      {activeUser &&
+                        activeUser.length > 0 &&
+                        activeUser.some (u => u.userId === currentfriend._id)
+                        ? <div className="active-icon" />
+                        : ''}
+
+                    </div>
+                    <div className="name">
+                      <h3>{currentfriend.userName} </h3>
+
+                    </div>
+                    
+                  
+
+                    
                   </div>
-                  <div className="name">
-                    <h3>{currentfriend.userName} </h3>
+
+                  <div className="icons">
+                    {/* <div className="icon">
+                      <FaPhoneAlt />
+                    </div> */}
+
+                    {/* <div className="icon">
+                      <FaVideo />
+                    </div> */}
+
+                    <div className="icon">
+                      <label htmlFor="dot"> <AiOutlineMenuFold /> </label>
+                    </div>
+                    {/* <div className="icon">
+                      <label htmlFor="dot2"> <FaRocketchat /> </label>
+                    </div> */}
 
                   </div>
                 </div>
 
-                <div className="icons">
-                  <div className="icon">
-                    <FaPhoneAlt />
-                  </div>
+                <Message
+                  message={message}
+                  currentfriend={currentfriend}
+                  scrollRef={scrollRef}
+                  typingMessage={typingMessage}
+                />
 
-                  <div className="icon">
-                    <FaVideo />
-                  </div>
+                <MessageSend
+                  inputHendle={inputHendle}
+                  newMessage={newMessage}
+                  sendMessage={sendMessage}
+                  emojiSend={emojiSend}
+                  ImageSend={ImageSend}
+                />
 
-                  <div className="icon">
-                    <label htmlFor="dot"> <FaRocketchat /> </label>
-                  </div>
-
-                </div>
               </div>
+            </div>
 
-              <Message
+            <div className="col-4">
+              <FriendInfo
                 message={message}
                 currentfriend={currentfriend}
-                scrollRef={scrollRef}
-                typingMessage={typingMessage}
+                activeUser={activeUser}
               />
-
-              <MessageSend
-                inputHendle={inputHendle}
-                newMessage={newMessage}
-                sendMessage={sendMessage}
-                emojiSend={emojiSend}
-                ImageSend={ImageSend}
-              />
-
             </div>
-          </div>
 
-          <div className="col-4">
-            <FriendInfo
-              message={message}
-              currentfriend={currentfriend}
-              activeUser={activeUser}
-            />
           </div>
-
         </div>
       </div>
     </div>
