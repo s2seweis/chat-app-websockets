25c34c7f6bcb0bbca628936d1df039a524781007
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-28 20:09:27 +0100

    upload

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-const express = require ('express');
-const app = express ();
-const dotenv = require ('dotenv');
-const http = require ('http');
-const cors = require("cors");
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const http = require('http');
+const cors = require('cors');
 
 // ################################################################################
-const server = http.createServer (app);
+const server = http.createServer(app);
 // ###
 
+// Use CORS middleware for Express routes
 app.use(cors());
 
-const io = require ('socket.io') (server, {
+const io = require('socket.io')(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
@@ -20,111 +21,70 @@ const io = require ('socket.io') (server, {
 let users = [];
 
 const addUser = (userId, socketId, userInfo) => {
-  const checkUser = users.some (u => u.userId === userId);
+  const checkUser = users.some(u => u.userId === userId);
 
   if (!checkUser) {
-    users.push ({userId, socketId, userInfo});
+    users.push({ userId, socketId, userInfo });
   }
 };
 
 const userRemove = socketId => {
-  users = users.filter (u => u.socketId !== socketId);
+  users = users.filter(u => u.socketId !== socketId);
 };
 
 const findFriend = id => {
-  return users.find (u => u.userId === id);
+  return users.find(u => u.userId === id);
 };
 
 const userLogout = userId => {
-  users = users.filter (u => u.userId !== userId);
+  users = users.filter(u => u.userId !== userId);
 };
 
-io.on ('connection', socket => {
-  console.log ('Socket is connecting...');
-  socket.on ('addUser', (userId, userInfo) => {
-    addUser (userId, socket.id, userInfo);
-    io.emit ('getUser', users);
+io.on('connection', socket => {
+  console.log('Socket is connecting...');
+  socket.on('addUser', (userId, userInfo) => {
+    addUser(userId, socket.id, userInfo);
+    io.emit('getUser', users);
 
-    const us = users.filter (u => u.userId !== userId);
+    const us = users.filter(u => u.userId !== userId);
     const con = 'new_user_add';
     for (var i = 0; i < us.length; i++) {
-      socket.to (us[i].socketId).emit ('new_user_add', con);
+      socket.to(us[i].socketId).emit('new_user_add', con);
     }
   });
-  socket.on ('sendMessage', data => {
-    const user = findFriend (data.reseverId);
+  // ... rest of your socket.io event handlers
 
-    if (user !== undefined) {
-      socket.to (user.socketId).emit ('getMessage', data);
-    }
-  });
-
-  socket.on ('messageSeen', msg => {
-    const user = findFriend (msg.senderId);
-    if (user !== undefined) {
-      socket.to (user.socketId).emit ('msgSeenResponse', msg);
-    }
-  });
-
-  socket.on ('delivaredMessage', msg => {
-    const user = findFriend (msg.senderId);
-    if (user !== undefined) {
-      socket.to (user.socketId).emit ('msgDelivaredResponse', msg);
-    }
-  });
-  socket.on ('seen', data => {
-    const user = findFriend (data.senderId);
-    if (user !== undefined) {
-      socket.to (user.socketId).emit ('seenSuccess', data);
-    }
-  });
-
-  socket.on ('typingMessage', data => {
-    const user = findFriend (data.reseverId);
-    if (user !== undefined) {
-      socket.to (user.socketId).emit ('typingMessageGet', {
-        senderId: data.senderId,
-        reseverId: data.reseverId,
-        msg: data.msg,
-      });
-    }
-  });
-
-  socket.on ('logout', userId => {
-    userLogout (userId);
-  });
-
-  socket.on ('disconnect', () => {
-    console.log ('user is disconnect... ');
-    userRemove (socket.id);
-    io.emit ('getUser', users);
+  socket.on('disconnect', () => {
+    console.log('user is disconnect... ');
+    userRemove(socket.id);
+    io.emit('getUser', users);
   });
 });
 
 // #########################################################################
 
-const databaseConnect = require ('./config/database');
-const authRouter = require ('./routes/authRoute');
-const bodyParser = require ('body-parser');
-const cookieParser = require ('cookie-parser');
-const messengerRoute = require ('./routes/messengerRoute');
+const databaseConnect = require('./config/database');
+const authRouter = require('./routes/authRoute');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const messengerRoute = require('./routes/messengerRoute');
 
-dotenv.config ({
+dotenv.config({
   path: 'config/config.env',
 });
 
-app.use (bodyParser.json ());
-app.use (cookieParser ());
-app.use ('/api/messenger', authRouter);
-app.use ('/api/messenger', messengerRoute);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use('/api/messenger', authRouter);
+app.use('/api/messenger', messengerRoute);
 
 const PORT = process.env.PORT || 5000;
-app.get ('/', (req, res) => {
-  res.send ('This is from backend Sever');
+app.get('/', (req, res) => {
+  res.send('This is from backend Sever');
 });
 
-databaseConnect ();
+databaseConnect();
 
-server.listen (PORT, () => {
-  console.log (`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
