f963d3b932322ff4de1fa73177cfffbac172e02b
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-28 23:28:06 +0100

    upload

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require ('express');
 const app = express ();
 const dotenv = require ('dotenv');
 const http = require ('http');
+const cors = require("cors");
 
 // ################################################################################
+app.use(cors());
+
 const server = http.createServer (app);
 // ###
 
