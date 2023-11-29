69b669479f4ce220949ea52547954b5ce727ef56
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-29 09:45:45 +0100

    upload

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,55 +1,17 @@
-// const jwt = require('jsonwebtoken');
-
-// module.exports.authMiddleware = async(req,res,next) => {
-//      const {authToken} = req.cookies;
-//      console.log("line:800", authToken);
-//      if(authToken){
-//           const deCodeToken = await jwt.verify(authToken,process.env.SECRET);
-//           req.myId = deCodeToken.id;
-//           next();
-//      }else{
-//           res.status(400).json({
-//                error:{
-//                     errorMessage: ['Please Loging First']
-//                }
-//           })
-//      } 
-// }
-
 const jwt = require('jsonwebtoken');
 
-module.exports.authMiddleware = async (req, res, next) => {
-  const authTokenHeader = req.headers.authorization;
-  console.log("line:900", authTokenHeader);
-
-  if (authTokenHeader) {
-    // Check if the header starts with 'Bearer'
-    if (authTokenHeader.startsWith('Bearer')) {
-      const authToken = authTokenHeader.slice(7); // Remove 'Bearer ' to get the token
-
-      try {
-        const decodedToken = jwt.verify(authToken, process.env.SECRET);
-        req.myId = decodedToken.id;
-        next();
-      } catch (error) {
-        res.status(401).json({
-          error: {
-            errorMessage: ['Invalid token'],
-          },
-        });
-      }
-    } else {
-      res.status(401).json({
-        error: {
-          errorMessage: ['Invalid authorization header format'],
-        },
-      });
-    }
-  } else {
-    res.status(401).json({
-      error: {
-        errorMessage: ['Authorization header missing'],
-      },
-    });
-  }
-};
+module.exports.authMiddleware = async(req,res,next) => {
+     const {authToken} = req.headers;
+     console.log("line:800", authToken);
+     if(authToken){
+          const deCodeToken = await jwt.verify(authToken,process.env.SECRET);
+          req.myId = deCodeToken.id;
+          next();
+     }else{
+          res.status(400).json({
+               error:{
+                    errorMessage: ['Please Loging First']
+               }
+          })
+     } 
+}
