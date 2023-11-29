583092dbe17d281e1f79d78910bc39c18483b4bc
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-29 03:25:07 +0100

    next

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async(req,res,next) => {
-     const {authToken} = req.cookies;
+     const {authToken} = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjMyOGQyMWY5Nzc4NWU2YWZiMDQyOSIsImVtYWlsIjoic2ViYXN0aWFuQGdtYWlsLmNvbSIsInVzZXJOYW1lIjoiU2ViYXN0aWFuIiwiaW1hZ2UiOiI4NzY4NzIuanBlZyIsInJlZ2lzdGVyVGltZSI6IjIwMjMtMTEtMjZUMTE6MTU6MzAuOTM0WiIsImlhdCI6MTcwMTIyMzUwOCwiZXhwIjoxNzAxODI4MzA4fQ.VqlKX6IsUshJjMMAdzuE8MkGKA0Eqhqray9-FGckz3E'
+     // const {authToken} = req.cookies;
      console.log("line:801", authToken);
      if(authToken){
           const deCodeToken = await jwt.verify(authToken,process.env.SECRET);
