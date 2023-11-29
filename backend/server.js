d5f7359ec02f998ca19b9191b6f2a78bbf35791d
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-29 01:21:28 +0100

    push

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer (app);
 // ###
 
-const io = require ('socket.io') (PORT, {
+const io = require ('socket.io') (server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
