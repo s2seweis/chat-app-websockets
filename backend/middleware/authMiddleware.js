7c7115d10ef120d127e70052a4d00222d99bd89a
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-29 03:29:35 +0100

    upload

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async(req,res,next) => {
-     const {authToken} = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NjMyOGQyMWY5Nzc4NWU2YWZiMDQyOSIsImVtYWlsIjoic2ViYXN0aWFuQGdtYWlsLmNvbSIsInVzZXJOYW1lIjoiU2ViYXN0aWFuIiwiaW1hZ2UiOiI4NzY4NzIuanBlZyIsInJlZ2lzdGVyVGltZSI6IjIwMjMtMTEtMjZUMTE6MTU6MzAuOTM0WiIsImlhdCI6MTcwMTIyMzUwOCwiZXhwIjoxNzAxODI4MzA4fQ.VqlKX6IsUshJjMMAdzuE8MkGKA0Eqhqray9-FGckz3E'
-     // const {authToken} = req.cookies;
-     console.log("line:801", authToken);
+     const {authToken} = req.cookies;
+     console.log("line:800", authToken);
      if(authToken){
           const deCodeToken = await jwt.verify(authToken,process.env.SECRET);
           req.myId = deCodeToken.id;
@@ -11,7 +10,7 @@ module.exports.authMiddleware = async(req,res,next) => {
      }else{
           res.status(400).json({
                error:{
-                    errorMessage: ['Please Login First']
+                    errorMessage: ['Please Loging First']
                }
           })
      } 
