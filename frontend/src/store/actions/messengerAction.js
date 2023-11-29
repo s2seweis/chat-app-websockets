5b2a8e4ce3f53b426a764841bd2d0189567bd86f
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-29 01:37:29 +0100

    next

--- frontend/src/store/actions/messengerAction.js
+++ frontend/src/store/actions/messengerAction.js
@@ -35,7 +35,7 @@ export const messageSend = (data) => async(dispatch) => {
 export const getMessage = (id) => {
      return async(dispatch) => {
           try{
-               const response = await axios.get(`https://react-app-chat-c986801b6d65.herokuapp.com/api/messenger/get-message/${id}`)
+               const response = await axios.get(`/api/messenger/get-message/${id}`)
               dispatch({
                    type : MESSAGE_GET_SUCCESS,
                    payload : {
