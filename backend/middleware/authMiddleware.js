61fd5e3f0e6adcfebd3e403f329f7e19379f43ab
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2023-11-29 09:31:58 +0100

    next

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -20,6 +20,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authMiddleware = async (req, res, next) => {
   const authTokenHeader = req.headers.authorization;
+  console.log("line:900", authTokenHeader);
 
   if (authTokenHeader) {
     // Check if the header starts with 'Bearer'
